e206fd075f3ed58dc73b48bc24ce25955e71aca8
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-02 11:34:16 +0300

    Create Box.jsx

---
+++ src/components/Box/Box.jsx
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+import { color, layout, space, border, typography, flexbox } from 'styled-system';
+
+export  const Box = styled('div')(color, space, layout, border, typography, flexbox)
+
