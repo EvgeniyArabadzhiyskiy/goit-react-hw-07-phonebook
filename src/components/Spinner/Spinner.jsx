6fac13d9c4dda9d9e4ff8c514f5819a1ae3ca76b
EvgeniyArabadzhiyskiy <evgeniyarabadziyskiy@gmail.com>
2022-08-04 17:06:22 +0300

    Update Spinner.jsx

--- src/components/Spinner/Spinner.jsx
+++ src/components/Spinner/Spinner.jsx
@@ -2,6 +2,14 @@ import styled from 'styled-components';
 import { keyframes } from 'styled-components';
 import { ImSpinner9 } from 'react-icons/im';
 
+export const Spinner = ({ size }) => {
+  return (
+    <>
+      <StyledSpinner size={size} />
+    </>
+  );
+};
+
 const spin = keyframes`
 0% {
   transform: rotate(0deg);
@@ -11,6 +19,6 @@ const spin = keyframes`
 }
 `;
 
-export const Spinner = styled(ImSpinner9)`
+export const StyledSpinner = styled(ImSpinner9)`
   animation: ${spin} 1s linear infinite;
 `;
